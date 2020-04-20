c4843095016a65b066bb1f784ded04904965e106
Carlos Blandino <cablandino66@gmail.com>
2020-04-20 13:29:03 -0700

    modifying code to reflect an object and adding draw card functionality

--- deck-of-card.js
+++ deck-of-card.js
@@ -1,22 +1,44 @@
 // Dynamically create a deck of playing cards
-
-function makeDeck() {
+const deckOfCards = {
     // Need to hold new cards in variable
-    const deck = [];
+    deck: [],
     // Hold the data of a card in different arrays
-    const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+    suits: ['hearts', 'diamonds', 'spades', 'clubs'],
     // Using a string for the values because it was faster to write
-
-    const values = '2,3,4,5,6,7,8,9,10,J,Q,K,A';
-    // Use split method on the string to separate from commas and return with array
-    // Iterate thru the values
-    for (let value of values.split(',')) {
-        // for each value you want to iterate thru the suits to add the value to each
-        for (let suit of suits) {
-            // In each iteration you want to add the card object to the deck
-            deck.push({ value, suit });
+    values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
+    // Need to hold drawn cards since the deck will lose the card
+    drawnCards: [],
+    makeDeck() {
+        const { suits, values, deck } = this;
+        // Use split method on the string to separate from commas and return with array
+        // Iterate thru the values
+        for (let value of values.split(',')) {
+            // for each value you want to iterate thru the suits to add the value to each
+            for (let suit of suits) {
+                // In each iteration you want to add the card object to the deck
+                deck.push({ value, suit });
+            }
         }
-    }
+        return deck;
+    },
 
-    return deck;
-}
+    drawCard() {
+        // Hold the card from the deck pop  and add it to the drawn cards array
+        const card = this.deck.pop();
+        this.drawnCards.push(card);
+        // Also return the card
+        return card;
+    },
+    drawCards(numberOfCards) {
+        // Need to hold the drawn cards
+        const cards = [];
+        // Iterate thru the number of cards
+        for (let card of numberOfCards) {
+            // In each iteration place the card in to the drawn card pile
+            cards.push(this.drawCard);
+        }
+        return cards;
+
+        // After iterating return the drawn cards
+    }
+};
