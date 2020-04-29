564726fd9113fc39b6ab38f684be06cb71d212b1
Carlos Blandino <cablandino66@gmail.com>
2020-04-29 09:27:14 -0700

    adding the function to the file

---
+++ reverse-an-array.js
@@ -0,0 +1,24 @@
+// Total Reverse
+
+// Build a function that takes in an array and reverses it
+
+// Write a function that takes one parameter
+// Parameter 1 - An array of anything
+// The function should output an array that is in reverse order from the input array
+// Example:
+// var myArray = ['hello', 45, 'Bob', 'what', '23'];
+// arrayReverse(myArray);
+// Output - ['23', 'what', 'Bob', 45, 'hello']
+// Avoid using pre-built functions that do something similar
+
+var myArray = ['hello', 45, 'Bob', 'what', '23'];
+
+function reverseArray(inputArray) {
+    var tempArray = [];
+    var arrayLen = inputArray.length - 1;
+    for (arrayLen; arrayLen >= 0; arrayLen--) {
+        tempArray.push(inputArray[arrayLen]);
+    }
+    return tempArray;
+}
+console.log(reverseArray(myArray));
