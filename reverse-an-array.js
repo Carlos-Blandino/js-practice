2d7ab809364830ef2e2dabc2ea002f90c56d6fc9
Carlos Blandino <cablandino66@gmail.com>
2020-06-22 12:17:53 -0700

    added a reverse word function an modified reverse an array function

--- reverse-an-array.js
+++ reverse-an-array.js
@@ -11,25 +11,35 @@
 // Output - ['23', 'what', 'Bob', 45, 'hello']
 // Avoid using pre-built functions that do something similar
 
-var myArray = ['hello', 45, 'Bob', 'what', '23'];
+var myArray = [ 'hello', 45, 'Bob', 'what', '23' ];
 
 function reverseArray(inputArray) {
-    var tempArray = [];
-    var arrayLen = inputArray.length - 1;
-    for (arrayLen; arrayLen >= 0; arrayLen--) {
-        tempArray.push(inputArray[arrayLen]);
-    }
-    return tempArray;
+	var tempArray = [];
+	var arrayLen = inputArray.length - 1;
+	for (arrayLen; arrayLen >= 0; arrayLen--) {
+		tempArray.push(inputArray[arrayLen]);
+	}
+	return tempArray;
 }
-console.log(reverseArray(myArray));
 
 function reverseAnArray(inputArray) {
-    let tempArray = [];
-    let tempStr = ' ';
-    for (let i = 0; i < inputArray.length; i++) {
-        tempStr = ' ' + inputArray[i] + tempStr;
-    }
-    return tempStr.trim().split(' ');
+	let tempArray = [];
+	//let tempStr = ' ';
+	for (let i = 0; i < inputArray.length; i++) {
+		//tempStr = ' ' + inputArray[i] + tempStr;
+		tempArray.unshift(inputArray[i]);
+	}
+	//return tempStr.trim().split(' ');
+	return tempArray;
 }
 
+function reverseWord(word) {
+	let reverseStr = '';
+	for (let i = 0; i < word.length; i++) {
+		reverseStr = word[i] + reverseStr;
+	}
+	return reverseStr;
+}
+
+console.log(reverseWord('Hello'));
 console.log(reverseAnArray(myArray));
