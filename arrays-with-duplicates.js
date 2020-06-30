b2e432f9f1e5f83467f08a610e94a2e846844f1f
Carlos Blandino <cablandino66@gmail.com>
2020-06-30 13:55:41 -0700

    completing functionality of arrays with duplicates

--- arrays-with-duplicates.js
+++ arrays-with-duplicates.js
@@ -1,17 +1,23 @@
-function arrayDuplicates(nums) {
+function deletingDuplicatesInArray(items) {
     let resultArray = []
-    let currentNum = nums[0]
-    nums.unshift()
+    let currentNum = items[0]
+    items.unshift()
 
-    for( let i = 0; i < nums.length; i++) {
-        if ( currentNum === nums[i]){
-            if (currentNum.includes(nums[i])){
-                continue;
-            } else {
-                resultArray.push(nums[i])
-                nums.unshift()
+    for( let i = 0; i < items.length; i++) {
+        for( let j = 0 ; j < items.length; j++){
+            if (currentNum === items[j]){
+                if (resultArray.includes(items[i])){
+                    items.unshift()
+                } else {
+                    resultArray.push(items[i])
+                    currentNum = items[i]
+                    items.unshift()
+                }
             }
         }
-    }
-}
 
+    }
+    return resultArray
+}
+console.log(deletingDuplicatesInArray([1,1,3,4,5,99,6,23,6,6,5,8,10]))
+console.log(deletingDuplicatesInArray(['dog','cat','mouse','cat','elephant','dog']))
