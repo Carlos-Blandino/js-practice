bfbf4b3f68bdb88772a173dc62e5597dc627edd8
Carlos Blandino <cablandino66@gmail.com>
2021-05-01 10:27:42 -0700

    working on binary search

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+function binarySearch(array, value){
+    // add whatever parameters you deem necessary - good luck!
+    let lowerBound = 0;
+    let upperBound = array.length - 1;
+    let middlePointer = Math.floor((lowerBound + upperBound )/ 2);
+    
+    while(lowerBound < upperBound){
+        if(value > array[middlePointer]){
+            lowerBound = middlePointer + 1;
+            middlePointer = Math.floor((lowerBound + upperBound )/ 2);
+        }
+        if(value < array[middlePointer]){
+            upperBound = middlePointer - 1;
+            middlePointer = Math.floor((lowerBound + upperBound )/ 2);
+        }
+        if(value === array[middlePointer]){
+            return middlePointer
+        }
+        
+    }
+    return -1
+  }
+  console.log('search', binarySearch([1,2,3,4,5],5))
